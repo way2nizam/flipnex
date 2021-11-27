226936924751d8fe408a12f393230b4580ef1f8d
Nizam <nizamy710@gmail.com>
2021-11-27 14:34:01 +0530

    feat: title added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <h1>Home Page</h1>
+    </div>
+  );
 }
 
 export default App;
