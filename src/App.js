48588fc5dc2b9f040b5096905b2ef766013b2a7f
Nizam <nizamy710@gmail.com>
2021-11-27 15:19:16 +0530

    header and footer component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>Home Page</h1>
+      <Footer />
     </div>
   );
 }
